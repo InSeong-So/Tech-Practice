6e014b973e0b0b2933074822e8352966bdf18ab9
Tene <tene@nextunicorn.kr>
2022-06-24 00:08:46 +0900

    🔬Chore: 의존성 설정

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// https://www.npmjs.com/package/node-json-db
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3003);
